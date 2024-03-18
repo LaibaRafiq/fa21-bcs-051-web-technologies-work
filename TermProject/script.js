be68b8a0fe72746319fa8eb1b0623adcadf8b096
LaibaRafiq <laibaayy0610@gmail.com>
2024-03-18 23:42:02 +0500

    update

--- TermProject/script.js
+++ TermProject/script.js
@@ -1,9 +1,18 @@
+const navEl= document.querySelector('.navbar');
+window.addEventListener('scroll',()=> {
+  if(window.scrollY>=45){
+    navEl.classList.add('navbar-scrolled')
+  }
+  else if(window.scrollY <45){
+    navEl.classList.remove('navbar-scrolled')
+  }
+})
+
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
 const btns = document.querySelectorAll(".nav-btn");
 const slides = document.querySelectorAll(".img-slide");
 const contents = document.querySelectorAll(".content");
-const inputs = document.querySelectorAll(".input");
 let sliderNav = function(manual) {
     btns.forEach((btn) => {
         btn.classList.remove("active");
@@ -27,19 +36,34 @@ btns.forEach((btn, i) => {
         sliderNav(i);
     });
 });
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
 
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
+let countDown = () => {
+  let futureDate = new Date("1 Jan 2025");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+  
+  if (myDate < 0) {
+    daysItem.innerHTML = 0;
+    hoursItem.innerHTML = 0;
+    minItem.innerHTML = 0;
+    secItem.innerHTML = 0;
+    return; 
   }
+
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  daysItem.innerHTML = days;
+  hoursItem.innerHTML = hours;
+  minItem.innerHTML = min;
+  secItem.innerHTML = sec;
 }
 
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
+countDown();
+setInterval(countDown, 1000);
