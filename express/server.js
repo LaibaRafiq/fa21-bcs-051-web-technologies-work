9f5fc0e66414ad7e2acfc678b2e241f369f5af1f
LaibaRafiq <laibaayy0610@gmail.com>
2024-05-05 21:54:09 +0500

    layout-routes

--- express/server.js
+++ express/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+// const Mobile = require("./models/Mobile");
 let server = express();
-server.set("view engine","ejs");
+server.use(express.json());
+server.set("view engine", "ejs");
 server.use(express.static("public"));
+var expressLayouts = require("express-ejs-layouts");
+server.use(expressLayouts);
 
-server.get("/contact", function(req,res){    
-    res.render("contact");
-})
-server.get("/", function(req,res){
-   
-    res.render("homepage");
-})
+let mobileApiRouter = require("./routes/api/mobiles");
+server.use("/", mobileApiRouter);
+server.use("/", require("./routes/api/games"));
+server.use("/", require("./routes/site/games"));
 
-server.listen(3000);
+server.get("/contact.html", (req, res) => {
+  res.render("contact");
+});
+server.get("/", (req, res) => {
+  res.render("homepage");
+});
+
+mongoose.connect("mongodb://localhost:27017").then((data) => {
+  console.log("DB Connected");
+});
+server.listen(3000, () => {
+  console.log("Server started at localhost:3000");
+});
